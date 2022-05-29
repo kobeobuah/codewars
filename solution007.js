3a62368d8c57b060b9e845b5471eef3f61ef1c6a
Richmond Obuah <kobeobuah@gmail.com>
2022-05-29 19:23:08 +0000

    added new solutions to codewars

---
+++ solution007.js
@@ -0,0 +1,19 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+function maps(x){
+    let double = [];
+    for (let i = 0; i < x.length; i++) {
+      double.push(x[i] * 2);
+    };
+    return double;
+  }
+
+
+  function maps(x){
+    return x.map(n => n * 2);
+  }

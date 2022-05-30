72571f5d183d82249212c1f680af17aca79f33e2
Richmond Obuah <kobeobuah@gmail.com>
2022-05-30 20:35:35 +0000

    added another codewars challenge

---
+++ solution008.js
@@ -0,0 +1,27 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+
+function removeEveryOther(arr){
+    //your code here
+    let keeps = []
+    for(let i = 0; i<arr.length; i = i+2){
+      keeps.push(arr[i])
+    }
+    return keeps
+  }
+
+
+  function removeEveryOther(arr){
+    return arr.filter(function(elem, index) {
+      return index % 2 === 0;
+    });
+  }
+
+
+  const removeEveryOther = arr => arr.filter((item, i) =>  i % 2 == 0);

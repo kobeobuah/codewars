e543de9e93fe9d7fc9e01e931833ab80a917bdd8
Richmond Obuah <kobeobuah@gmail.com>
2022-05-31 18:58:53 +0000

    added new solutions to codewars challenge

--- solution009.js
+++ solution009.js
@@ -9,3 +9,30 @@
 smash = function (words) {
     return words.join(" ");
   };
+
+
+//   Write a method, that will get an integer array as parameter and will process every number from this array.
+
+//   Return a new array with processing every number of the input-array like this:
+  
+//   If the number has an integer square root, take this, otherwise square the number.
+//   Example
+  
+//   [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+  
+//   Notes
+  
+//   The input array will always contain only positive numbers, and will never be empty or null.
+  
+
+function squareOrSquareRoot(array) {
+    let arr = array
+    for (i = 0; i < array.length; i++){
+      if (Number.isInteger(Math.sqrt(arr[i])) ){
+        arr[i] = Math.sqrt(arr[i])
+      } 
+      else {
+        arr[i] = arr[i] * arr[i] 
+      }
+    } return arr;  
+  }

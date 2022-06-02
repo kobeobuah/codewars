f8e2008290fc311c97f628f2a7f05cc1ede28d99
Richmond Obuah <kobeobuah@gmail.com>
2022-06-02 16:16:16 +0000

    new solutions added to codewars challenge

--- solution010.js
+++ solution010.js
@@ -9,7 +9,7 @@
 
 
 function countPositivesSumNegatives(input) {
-    if (input == null || input.length == 0)
+    if (input === null || input.length === 0)
       return [];
     
     var positive = 0;

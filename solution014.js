5722f1eda02b3d8a0399db8a01907661be903ab4
Richmond Obuah <kobeobuah@gmail.com>
2022-06-05 19:47:54 +0000

    added new codewars solutions to challenge

---
+++ solution014.js
@@ -0,0 +1,20 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+// Examples (Input -> Output):
+
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+// Good Luck!
+
+
+function doubleChar (str) {
+    let newString = ""
+    for(let i = 0; i < str.length; i++){
+        newString += str[i] + str[i]
+    }
+    return newString;
+}
+
+
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
